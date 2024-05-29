2b49c72764a981f25af38c1223dea1abded22a36
Fritz Lehwalder <54349544+FritzLehwalder@users.noreply.github.com>
2024-05-28 20:19:11 -0600

    Update autoclick.cpp

--- autoclick.cpp
+++ autoclick.cpp
@@ -1,32 +1,58 @@
 #include <windows.h>
 #include <iostream>
+
 bool started = false;
 int timer = 10;
-void start() {
-    std:cout << "Enter ms between click: \t";
-    std:cin >> timer;
+int x = 0;
+int y = 0;
+bool customPos = false;
+POINT curserPos;
 
-    // testing
-    std:cout >> timer;
-
-    std:cout << "\n";'
-    while (true) {
-        if (GetAsyncKeyState(VK_F10)) {
-            started = !started;
-            if(started) std::cout << "Autoclicker has started.\n";
-            if(!started) std::cout << "Autoclicker has stopped.\n";
-            Sleep(400);
-        }
-        if (started) {
-            mouse_event(MOUSEEVENTF_LEFTDOWN, x, y, 0, 0);
-            mouse_event(MOUSEEVENTF_LEFTUP, x, y, 0, 0);
-            Sleep(timer);
-        }
+static void start() {
+while (true) {
+    if (GetAsyncKeyState(VK_PAUSE)) {
+        started = !started;
+        if (started) std::cout << "Autoclicker has started.\n";
+        if (!started) std::cout << "Autoclicker has stopped.\n";
+        Sleep(400);
     }
+    if (GetAsyncKeyState(VK_INSERT)) {
+        customPos = !customPos;
+        if (customPos) {
+            GetCursorPos(&curserPos);
+            x = curserPos.x;
+            y = curserPos.y;
+            std::cout << "Clicking position set to " << curserPos.x << ", " << curserPos.y << ".\n";
+        }
+        if (!customPos) std::cout << "Click position set to mouse.\n";
+
+        Sleep(400);
+    }
+    if (started) {
+        
+        if (!customPos) {
+            GetCursorPos(&curserPos);
+            x = curserPos.x;
+            y = curserPos.y;
+        }
+        else {
+            SetCursorPos(x, y);
+        }
+
+        mouse_event(MOUSEEVENTF_LEFTDOWN, x, y, 0, 0);
+        mouse_event(MOUSEEVENTF_LEFTUP, x, y, 0, 0);
+        Sleep(timer);
+    }
+}
 }
 
 int main()
 {
-    std::cout << "Press F10 to toggle autoclicker.\n";
+    std::cout << "Enter ms between click: ";
+    std::cin >> timer;
+    std::cout << "===============================================\n";
+    std::cout << "Press PAUSE to toggle autoclicker.\n";
+    std::cout << "Press INSERT to toggle click on saved position.\n";
+    std::cout << "===============================================\n";
     start();
 }
